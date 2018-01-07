271a04cc738c29164c2e29d61cc495eba22aa469
GKKsport <35174753+GKKsport@users.noreply.github.com>
2018-01-07 17:38:26 +0100

    Triathlon-calc

---
+++ calc.js
@@ -0,0 +1,18 @@
+function onClick() {
+    var zwemmin = $("#zwemmenmin").val();
+    var zwemsec = $("#zwemmensec").val();
+    var loopmin = $("#lopenmin").val();
+    var loopsec = $("#lopensec").val();
+
+    var fietsen = $("#fietsen").val(); // fietssnelheid in km/h
+    var zwemmen = (+zwemmin * 60) + +zwemsec;
+    var lopen = (+loopmin * 60) + +loopsec;
+
+    var totaal = (zwemmen * 5) + (((18.8 * 60) / fietsen) * 60) + (lopen * 5.1);
+
+    var minuten = Math.floor(totaal / 60);
+    var seconden = totaal - (minuten * 60);
+
+    document.getElementById("totaaltijd").innerHTML = minuten + " minuten en " + Math.floor(seconden) + " seconden.";
+    return false;
+}
