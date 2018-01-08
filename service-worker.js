c73c0b19a8da949a30eab096b8fbbf3290db77da
GKKsport <dmdrumandbass@gmail.com>
2018-01-08 14:51:24 +0100

    kaartregels.html
    En serviceworker aangepast

--- service-worker.js
+++ service-worker.js
@@ -14,8 +14,13 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'GKKv1.0.0';
+const PRECACHE = 'GKKv1.0.1';
 const RUNTIME = 'runtime';
+
+//Update-log
+//1.0.0 - Eerste test-versie
+//1.0.1 - Aanvulling van kaartregels.html
+
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -23,6 +28,7 @@ const PRECACHE_URLS = [
     'triathloncalc.html',
     'tussenstand.html',
     'zwemmen.html',
+    'kaartregels.html',
   './', // Alias for index.html
   'calc.js',
   '../../styles/main.css',
