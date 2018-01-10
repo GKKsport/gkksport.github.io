6f00e1ca80cb5a70b43bb2dbcf5c904332fcfd27
GKKsport <35174753+GKKsport@users.noreply.github.com>
2018-01-10 18:42:44 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'GKKv1.0.4';
+const PRECACHE = 'GKKv1.0.5';
 const RUNTIME = 'runtime';
 
 //Update-log
@@ -23,6 +23,7 @@ const RUNTIME = 'runtime';
 //1.0.2 - Aanpassingen taalbalk (google) en wijzigingen op kaartregels.html
 //1.0.3 - Aanpassing serviceworker om main.css niet te laden
 //1.0.4 - Strop bij kalender
+//1.0.5 - ?op bijgevoegd voor reserveren badminton/tennis (anders AJAX error)
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
