d70af88131a987c6d057a48c715201aa64a4d5f4
GKKsport <dmdrumandbass@gmail.com>
2018-01-12 12:22:00 +0100

    Removed notifications

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'GKKv1.0.6';
+const PRECACHE = 'GKKv1.0.7';
 const RUNTIME = 'runtime';
 
 //Update-log
@@ -25,6 +25,7 @@ const RUNTIME = 'runtime';
 //1.0.4 - Strop bij kalender
 //1.0.5 - ?op bijgevoegd voor reserveren badminton/tennis (anders AJAX error)
 //1.0.6 - test met notifications 
+//1.0.7 - notifications removed
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
