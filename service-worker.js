9ef613d0b35baac5def89fb1709439f1cddc902f
GKKsport <dmdrumandbass@gmail.com>
2018-01-17 14:33:14 +0100

    V1 Ski-piste

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'GKKv1.0.9';
+const PRECACHE = 'GKKv1.1.0';
 const RUNTIME = 'runtime';
 
 //Update-log
@@ -28,6 +28,7 @@ const RUNTIME = 'runtime';
 //1.0.7 - notifications removed
 //1.0.8 - Initiële ski-piste update!
 //1.0.9 - Ski-pistes fix
+//1.1.0 - Eerste volledige versie van ski-piste checker 
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
