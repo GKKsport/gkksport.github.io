2b8ab0db1b02c3d029f555c6eae87088cff5326f
GKKsport <dmdrumandbass@gmail.com>
2018-02-01 23:11:19 +0100

    Infographic

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'GKKv1.2.2';
+const PRECACHE = 'GKKv1.2.3';
 const RUNTIME = 'runtime';
 
 //Update-log
@@ -36,6 +36,7 @@ const RUNTIME = 'runtime';
 //1.2.0 - Cache fix, klaar voor Skireis
 //1.2.1 - Millerflip
 //1.2.2 - Foto's bij grabs en grinds
+//1.2.3 - Infographic
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -51,7 +52,9 @@ const PRECACHE_URLS = [
     'img/grabs.jpg',
     'img/goofgrinds.jpg',
     'img/reggrinds.jpg',
-    'img/grabsgoof.jpg'
+    'img/grabsgoof.jpg',
+    'img/spins.jpeg',
+    'img/grinds.jpeg'
   // '../../styles/main.css' voor files in mappen
 ];
 
