91b9fa824eb64388ef83814c185dd61270eff297
GKKsport <dmdrumandbass@gmail.com>
2018-03-11 12:18:23 +0100

    Chat

--- service-worker.js
+++ service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'GKKv2.0.7';
-const RUNTIME = 'GKKv2.0.7';
+const PRECACHE = 'GKKv2.0.8';
+const RUNTIME = 'GKKv2.0.8';
 
 //Update-log
 //1.0.0 - Eerste test-versie
@@ -44,6 +44,7 @@ const RUNTIME = 'GKKv2.0.7';
 //2.0.4 - css-tweaks
 //2.0.6 - Triathlon-calc update (eigen afstand)
 //2.0.7 - Triathlon-countdown op tussenstandpagina
+//2.0.8 - Chat
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
