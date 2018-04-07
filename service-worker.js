42288ceff5c696ef2169adbe572de315f45e083d
GKKsport <dmdrumandbass@gmail.com>
2018-04-07 22:13:44 +0200

    Push

--- service-worker.js
+++ service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'GKKv3.0.1';
-const RUNTIME = 'GKKv3.0.1';
+const PRECACHE = 'GKKv3.0.2';
+const RUNTIME = 'GKKv3.0.2';
 
 //Update-log
 //1.0.0 - Eerste test-versie
