d891de43925586023df50a5da37eab35487bdd2b
simon <dmdrumandbass@gmail.com>
2018-05-28 18:17:26 +0200

    FIX

--- js/koers.js
+++ js/koers.js
@@ -129,11 +129,20 @@ function showInfo(data, tabletop) {
   // if (/SPRINT/i.test(data[i].gapsjersey))
 
   // Om truitjes toe te voegen
-  if (data[4].koers === "tour" && data[0].algnaam != "") {
-    document.body.innerHTML = document.body.innerHTML.split(data[0].algnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[6].koers + ";'></i> " + data[0].algnaam);
-    document.body.innerHTML = document.body.innerHTML.split(data[0].puntnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[7].koers + ";'></i> " + data[0].puntnaam);
-    document.body.innerHTML = document.body.innerHTML.split(data[0].bergnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[8].koers + ";'></i> " + data[0].bergnaam);
-    document.body.innerHTML = document.body.innerHTML.split(data[0].jongnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[9].koers + ";'></i> " + data[0].jongnaam);
+  if (data[4].koers === "tour") {
+    if (data[0].algnaam != "") {
+      document.body.innerHTML = document.body.innerHTML.split(data[0].algnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[6].koers + ";'></i> " + data[0].algnaam);
+    }
+    if (data[0].puntnaam != "") {
+      document.body.innerHTML = document.body.innerHTML.split(data[0].puntnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[7].koers + ";'></i> " + data[0].puntnaam);
+    }
+    if (data[0].bergnaam != "") {
+      document.body.innerHTML = document.body.innerHTML.split(data[0].bergnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[8].koers + ";'></i> " + data[0].bergnaam);
+    }
+    if (data[0].jongnaam != "") {
+      document.body.innerHTML = document.body.innerHTML.split(data[0].jongnaam).join("<i class='fas rand fa-tshirt' style='color:" + data[9].koers + ";'></i> " + data[0].jongnaam);
+
+    }
   }
   // Verandert de vlagtekst naar werkelijke vlagjes op de site
   vlaggen();
